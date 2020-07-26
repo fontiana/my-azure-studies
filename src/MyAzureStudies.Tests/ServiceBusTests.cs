34d2c83cdbb568dc5ebe4675aa01c2485cb6a7cc
victor.oliveira <victor.oliveira@avanade.com>
2020-07-26 13:00:24 -0300

    feat: adding tests

--- src/MyAzureStudies.Tests/ServiceBusTests.cs
+++ src/MyAzureStudies.Tests/ServiceBusTests.cs
@@ -1,17 +1,25 @@
 using System;
 using Xunit;
-using MyAzureStudies;
 using Xunit.Abstractions;
+using MyAzureStudies.ServiceBus;
 
 namespace MyAzureStudies.Tests
 {
     public class ServiceBusTests
     {
-        //private ITestOutputHelper testOutputHelper { get; }
+        private ITestOutputHelper testOutputHelper { get; }
 
-        [Fact]
-        public void Test1()
+        public ServiceBusTests(ITestOutputHelper testOutputHelper)
         {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact(DisplayName = "Test the default service bus behaviour")]
+        public void ShouldReceiveMessage()
+        {
+            var serviceBus = new Sample("", "", testOutputHelper);
+
+            serviceBus.Execute().GetAwaiter().GetResult();
         }
     }
 }
--- src/MyAzureStudies/ServiceBus/Sample.cs
+++ src/MyAzureStudies/ServiceBus/Sample.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
+using Xunit.Abstractions;
 
 namespace MyAzureStudies.ServiceBus
 {
@@ -13,24 +14,21 @@ namespace MyAzureStudies.ServiceBus
     /// </summary>
     public class Sample
     {
-        private string connectionString { get; }
-        private string queueName { get; }
-        //private ITestOutputHelper testOutputHelper { get; }
+        private ITestOutputHelper output { get; }
         private IQueueClient queueClient { get; }
 
-        public Sample(string connectionString, string queueName)
+        public Sample(string connectionString, string queueName, ITestOutputHelper output)
         {
-            this.connectionString = connectionString;
-            this.queueName = queueName;
-
+            this.output = output;
+            output.WriteLine("Initiaing QueueClient with default mode");
             queueClient = queueClient = new QueueClient(connectionString, queueName);
         }
 
         public async Task Execute()
         {
-            Console.WriteLine("======================================================");
-            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
-            Console.WriteLine("======================================================");
+            output.WriteLine("=========================================================");
+            output.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            output.WriteLine("=========================================================");
             
             RegisterOnMessageHandlerAndReceiveMessages();
 
@@ -58,7 +56,7 @@ namespace MyAzureStudies.ServiceBus
         private async Task ProcessMessagesAsync(Message message, CancellationToken token)
         {
             // Process the message.
-            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            output.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
 
             // Complete the message so that it is not received again.
             // This can be done only if the queue Client is created in ReceiveMode.PeekLock mode (which is the default).
@@ -69,15 +67,14 @@ namespace MyAzureStudies.ServiceBus
             // to avoid unnecessary exceptions.
         }
 
-        // Use this handler to examine the exceptions received on the message pump.
-        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
         {
-            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            output.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
             var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
-            Console.WriteLine("Exception context for troubleshooting:");
-            Console.WriteLine($"- Endpoint: {context.Endpoint}");
-            Console.WriteLine($"- Entity Path: {context.EntityPath}");
-            Console.WriteLine($"- Executing Action: {context.Action}");
+            output.WriteLine("Exception context for troubleshooting:");
+            output.WriteLine($"- Endpoint: {context.Endpoint}");
+            output.WriteLine($"- Entity Path: {context.EntityPath}");
+            output.WriteLine($"- Executing Action: {context.Action}");
             return Task.CompletedTask;
         }
     }
